4958cbb7e74497c394b939971190d5940bbcd240
Dawn Huang <q2379685@gmail.com>
2024-07-22 21:27:19 +0800

    [build]:驗證的使用解說

--- passport-pr/app/Http/Requests/CreateUser.php
+++ passport-pr/app/Http/Requests/CreateUser.php
@@ -13,6 +13,9 @@ class CreateUser extends FormRequest
      */
     public function authorize()
     {
+        // 這個方法用來決定當前用戶是否被授權發送此請求。
+        // 在這個示例中，簡單地返回 true，表示所有用戶都被授權。
+        // 如果您需要更細粒度的授權控制，可以在這裡添加自定義邏輯。
         return true;
     }
 
